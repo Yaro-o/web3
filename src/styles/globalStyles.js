6aa4fa39934685c81265b8fc537d08c8053703b5
Yaroo <42944920+Yaro-o@users.noreply.github.com>
2022-01-07 05:48:08 -0500

    Update globalStyles.js

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -54,19 +54,23 @@ export const TextTitle = styled.p`
   color: var(--primary-text);
   font-size: 22px;
   font-weight: 500;
+  
   line-height: 1.6;
 `;
 
 export const TextSubTitle = styled.p`
   color: var(--primary-text);
   font-size: 18px;
+  
   line-height: 1.6;
 `;
 
 export const TextDescription = styled.p`
-  color: var(--primary-text);
-  font-size: 16px;
+  color: var(--primary-texta);
+  font-size: 18px;
   line-height: 1.6;
+  
+  
 `;
 
 export const StyledClickable = styled.div`
